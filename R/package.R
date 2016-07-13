78ffc65a62f638a087b3db75c451d0a330629871
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-07-13 08:49:07 +0100

    Support for generating package code
    Fixes #15

---
+++ R/package.R
@@ -0,0 +1,92 @@
+##' Create an odin model within an existing package.
+##'
+##' I am resisiting the urge to actually create the package here.
+##' There are better options than I can come up with; for example
+##' \code{devtools::create}, \code{pkgkitten::kitten},
+##' \code{mason::mason}, or creating \code{DESCRIPTION} files using
+##' \code{desc}.  What is required here is that your package:
+##'
+##' \itemize{
+##' \item{Lists \code{odin} in \code{Imports:}}
+##' \item{Includes \code{useDynLib{<your package name>}} in
+##'   \code{NAMESPACE} (possibly via a roxygen comment \code{@useDynLib
+##'   <your package name>}}
+##' \item{To avoid a NOTE in \code{R CMD check}, import something from
+##'   \code{odin} in your namespace (e.g., \code{importFrom("odin", "odin")}
+##'   or roxygen \code{@importFrom(odin, odin)}}
+##' }
+##'
+##' Point this function at the package root (the directory containing
+##' \code{DESCRIPTION} and it will, by default, write out files
+##' \code{src/odin.c} and \code{odin.R}.  These files will be
+##' overwritten without warning by running this again.
+##'
+##' The generated models are activated in the package using
+##' \code{\link{delayedAssign}}.  This might prove challenging to
+##' document (especially using roxygen) but hopefully something will
+##' work out.
+##'
+##' Apart from the documentation, the generated code \emph{should}
+##' pass R CMD check without issue.
+##'
+##' @title Create odin model in a package
+##' @param path_package Path to the package root (the directory that
+##'   contains \code{DESCRIPTION})
+##' @param filenames A character vector of filenames containing odin
+##'   sources.  Alternatively, all .R files within the directory
+##'   \code{inst/odin} will be used.
+##' @param single_file Create a single file containing all models (the
+##'   default).  Set this to \code{FALSE} to create a series of files
+##'   all starting with \code{odin_}.
+##' @export
+odin_generate_package <- function(path_package, filenames=NULL,
+                                  single_file=TRUE) {
+  if (!file.exists(file.path(path_package, "DESCRIPTION"))) {
+    stop("Did not find package at ", path_package)
+  }
+  if (is.null(filenames)) {
+    inst_odin <- file.path(path_package, "inst/odin")
+    if (!is_directory(inst_odin)) {
+      stop("If 'filenames' is not given, inst/odin must exist")
+    }
+    filenames <- dir(inst_odin, pattern="\\.[Rr]", full.names=TRUE)
+  }
+  if (length(filenames) == 0L) {
+    stop("At least one filename must be given")
+  }
+  msg <- !file.exists(filenames)
+  if (any(msg)) {
+    stop("Input files not found:", paste(filenames[msg], collapse=", "))
+  }
+
+  dat <- lapply(filenames, function(f)
+    odin_generate(odin_parse(f, "file"), package=TRUE))
+  code <- vcapply(dat, "[[", "code")
+  base <- vcapply(dat, "[[", "base")
+
+  defn <- unique(unlist(lapply(dat, function(x) x$library_fns$definitions)))
+
+  header <- c(odin_header(), odin_includes())
+
+  dir.create(file.path(path_package, "R"), FALSE)
+  dir.create(file.path(path_package, "src"), FALSE)
+
+  writel <- function(x, file) {
+    writeLines(x, file.path(path_package, "src", file))
+  }
+
+  if (single_file) {
+    writel(c(header, "\n", code, "\n", defn), "odin.c")
+  } else {
+    for (i in seq_along(filenames)) {
+      writel(c(header, "\n", code[[i]]), sprintf("odin_%s.c", base[[i]]))
+      writel(c(header, "\n", defn), "odin.c")
+    }
+  }
+
+  r_code <- sprintf(
+    'delayedAssign("%s", odin::ode_system_generator(.packageName, "%s"))',
+    base, base)
+  writeLines(c(sub("^//", "##", odin_header()), r_code),
+             file.path(path_package, "R", "odin.R"))
+}
