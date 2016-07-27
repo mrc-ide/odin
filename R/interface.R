e04c804e05e8dbcab2bb8c40807ee3592b2c7200
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-07-27 17:28:56 +0100

    Better compilation testing
    No longer uses a file (just writes it in time), compiles in the
    temporary directory, new verbose option and cache skipping option.
    A diagnostic message is printed if compilation fails and gcc can't be
    found.

--- R/interface.R
+++ R/interface.R
@@ -133,14 +133,28 @@ odin_ <- function(x, dest=".", build=TRUE, load=TRUE, verbose=TRUE) {
 ##' examples, and tries compiling a trivial C program with \code{R CMD
 ##' SHLIB}.  Results are cached between runs within a session so this
 ##' should be fast to rely on.
+##'
+##' If this function believes you can't compile, and if \code{gcc}
+##' can't be found on the path, a diagnostc message will be printed.
+##' This will of course not be very interesting if you use a different
+##' compiler to gcc!  But the most likely people affected here are
+##' Windows users; if you get this ensure that you have rtools
+##' installed.  If you have \code{devtools} installed,
+##' \code{devtools::find_rtools()} may be helpful for diagnosing
+##' compiler issues.
+##'
 ##' @title Test if compilation is possible
+##' @param verbose Be verbose when running commands?
+##' @param skip_cache Try again to compile, skipping the cached value?
 ##' @return A logical scalar
 ##' @export
 ##' @examples
 ##' can_compile() # will take ~0.1s the first time
 ##' can_compile() # should be basically instantaneous
-can_compile <- function() {
-  if (is.null(getOption("odin.can_compile"))) {
+can_compile <- function(verbose=FALSE, skip_cache=FALSE) {
+  ## should offer a verbose option
+  ## should check if GCC is in the path?
+  if (skip_cache || is.null(getOption("odin.can_compile"))) {
     tmp <- tempfile()
     dir.create(tmp)
     owd <- setwd(tmp)
@@ -148,11 +162,16 @@ can_compile <- function() {
       setwd(owd)
       unlink(tmp, recursive=TRUE)
     })
-    file <- system.file("hello.c", package="odin", mustWork=TRUE)
+    file <- "hello.c"
+    writeLines("#include <R.h>", file)
+    dest <- if (verbose) "" else FALSE
     Sys.setenv(R_TESTS="")
     code <- system2(file.path(R.home(), "bin", "R"),
-                    c("CMD", "SHLIB", file),
-                    stdout=FALSE, stderr=FALSE)
+                    c("CMD", "SHLIB", "hello.c"),
+                    stdout=dest, stderr=dest)
+    if (code != 0L && Sys.which("gcc") == "") {
+      message("I don't see gcc on the PATH")
+    }
     options(odin.can_compile=code == 0L)
   }
   getOption("odin.can_compile", FALSE)
