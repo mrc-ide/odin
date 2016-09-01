97b5c44c4a2f71209604d3a1f685c28711be0b4c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-09-01 17:07:37 +0100

    Tests for the user c reading

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -5,3 +5,14 @@ test_that("can_compile", {
   expect_true(can_compile())
   expect_true(can_compile(skip_cache=TRUE))
 })
+
+test_that("read_user_c - simple", {
+  ans <- read_user_c("user_fns.c")
+  expect_equal(names(ans$declarations), "squarepulse")
+  expect_equal(names(ans$definitions), "squarepulse")
+})
+
+## This currently fails
+test_that("read_user_c - split declaration", {
+  expect_error(read_user_c("user_fns2.c"), "Parse error")
+})
---
+++ tests/testthat/user_fns2.c
@@ -0,0 +1,5 @@
+double squarepulse(double t,
+                   double t0,
+                   double t1) {
+  return t >= t0 && t < t1;
+}
