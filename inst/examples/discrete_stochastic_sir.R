63f0855ed92d923a58bc4b9b224af705761aaccc
Thibaut Jombart <thibautjombart@gmail.com>
2018-02-21 13:20:18 +0000

    vignette now has deterministic discrete sir model

---
+++ inst/examples/discrete_stochastic_sir.R
@@ -0,0 +1,20 @@
+
+## equations for transitions between compartments
+
+update(S) <- S - beta * S * I / N
+update(I) <- I + beta * S * I / N - gamma * I
+update(R) <- R + gamma * I
+N <- S + I + R
+
+
+## initial states
+
+initial(S) <- S_ini # will be user-defined
+initial(I) <- I_ini # will be user-defined
+initial(R) <- 0
+
+S_ini <- user(1000) # user-defined, default = 1000
+I_ini <- user(1) # user-defined, default = 1
+beta <- user(0.2) # user-defined, default =  0.2
+gamma <- user(0.1) # user-defined, default = 0.1
+
