5db4f2c0a8b0fef0d7805f37dd9e54d7c32760a9
Thibaut Jombart <thibautjombart@gmail.com>
2018-02-21 16:33:41 +0000

    draft of SEIRDS

--- vignettes/src/discrete.R
+++ vignettes/src/discrete.R
@@ -255,3 +255,79 @@ matplot(x_res[, 1], x_res[, -1], xlab = "Time", ylab = "Number of individuals",
         col = rep(transp(sir_col, .3), each = 100),
         lwd = 3, lty = 1)
 legend("left", lwd = 3, col = sir_col, legend = c("S", "I", "R"))
+
+
+
+
+
+## ## A discrete, stochastic SEIRDS model
+
+## This model is a more complex version of the previous one, which we will use
+## to illustrate the use of all distributions mentioned in the first part:
+## Binomial, Poisson and Multinomial.
+
+
+### Model description
+
+## The model is contains the following compartments:
+
+## - $S$: susceptibles
+## - $E$: exposed, i.e. infected but not yet contagious
+## - $I_R$: infectious who will survive
+## - $I_D$: infectious who will die
+## - $R$: recovered
+## - $D$: dead
+
+
+## There are no birth of natural death processes in this model. Parameters are:
+
+## - $\beta$: rate of infection
+## - $\delta$: rate at which symptoms appear (i.e inverse of mean incubation
+## period)
+## - $\gamma_R$: recovery rate
+## - $\gamma_D$: death rate
+## - $\mu$: case fatality ratio (proportion of cases who die)
+## - $\epsilon$: import rate of infected individuals (applies to $E$ and $I$)
+## - $\omega$: rate waning immunity
+
+
+## The model will be written as:
+
+## $$
+## S_{t+1} = S_t - \beta \frac{S_t (I_{R,t} + I_{D,t})}{N_t} + \omega R_t
+## $$
+
+## $$
+## E_{t+1} = E_t + \beta \frac{S_t (I_{R,t} + I_{D,t})}{N_t} - \delta E_t + \epsilon
+## $$
+
+## $$
+## I_{R,t+1} = I_{R,t} + \delta (1 - \mu) E_t  \gamma_R I_{R,t} + \epsilon
+## $$
+
+## $$
+## I_{D,t+1} = I_{D,t} + \delta \mu E_t  \gamma_D I_{D,t} + \epsilon
+## $$
+
+## $$
+## R_{t+1} = R_t + \gamma_R I_{R,t} - \omega R_t
+## $$
+
+## $$
+## D_{t+1} = D_t + \gamma_D I_{D,t}
+## $$
+
+
+
+
+## The formulation of the model in `odin` is:
+
+##+ load_sir_s
+path_seirds_model <- system.file("examples/discrete_stochastic_seirds.R", package = "odin")
+
+##+ echo = FALSE, results = "asis"
+cat(readLines(path_seirds_model), sep = "\n")
+
+seirds_model <- odin::odin(path_seirds_model, verbose = FALSE, skip_cache = TRUE)
+seirds_model
+x <- seirds_model()
