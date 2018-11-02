3c9f7699b923904295260e1e0a1b07881c059eb3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-11-02 15:36:17 +0000

    Add skeleton of a JSON ir

---
+++ R/ir.R
@@ -0,0 +1,84 @@
+odin_ir <- function(x, type = NULL, validate = FALSE) {
+  ## TODO: see comments in odin validate model - this might want to
+  ## flip around
+  res <- odin_validate_model(x, type)
+
+  dat <- res$result
+
+  ir_dat <- list(config = ir_config(dat),
+                 features = ir_features(dat),
+                 equations = ir_equations(dat))
+  ir <- jsonlite::toJSON(ir_dat, null = "null")
+  if (validate) {
+    ir_validate(ir, TRUE)
+  }
+  ir
+}
+
+
+ir_config <- function(dat) {
+  ## We should probably add here:
+  ##
+  ## - odin version
+  ## - file
+  ## - type
+  ## - path
+  ## - include?
+  list(base = jsonlite::unbox(dat$config$base))
+}
+
+
+ir_features <- function(dat) {
+  v <- c("discrete", "has_array", "has_output", "has_user", "has_delay",
+         "has_interpolate", "has_stochastic")
+  lapply(dat$info[v], jsonlite::unbox)
+}
+
+
+ir_equations <- function(dat) {
+  unname(lapply(dat$eqs, ir_equation))
+}
+
+
+ir_equation <- function(eq) {
+  lhs <- list(data_type = jsonlite::unbox(eq$lhs$data_type))
+  if (!is.null(lhs$special)) {
+    lhs$special <- jsonlite::unbox(eq$lhs$special)
+    lhs$target <- jsonlite::unbox(eq$lhs$target)
+  }
+
+  if (eq$rhs$type == "atomic") {
+    rhs <- list(type = jsonlite::unbox(eq$rhs$type),
+                value = jsonlite::unbox(eq$rhs$value))
+  } else {
+    stop("rhs type needs implementing")
+  }
+
+  list(name = jsonlite::unbox(eq$name),
+       source = list(expression = jsonlite::unbox(eq$expr_str),
+                     line = jsonlite::unbox(eq$line)),
+       stage = jsonlite::unbox(STAGES[[eq$stage]]),
+       stochastic = jsonlite::unbox(eq$stochastic),
+       lhs = lhs,
+       rhs = rhs)
+}
+
+
+## Eventually this should be cached within a session, but wait until
+## it works.
+ir_schema <- function() {
+  path <- system.file("schema.json", package = "odin", mustWork = TRUE)
+  dat <- readChar(path, file.size(path))
+  ## We get somewhat better errors from jsonlite's parsers than hoping
+  ## that the json is valid.
+  jsonlite::fromJSON(dat)
+  dat
+}
+
+
+ir_validate <- function(x, error = FALSE) {
+  engine <- if (packageVersion("jsonvalidate") > "1.0.0") "ajv" else "imjv"
+  jsonvalidate::json_validate(x, ir_schema(),
+                              verbose = TRUE, greedy = TRUE, error = error,
+                              engine = "imjv")
+}
