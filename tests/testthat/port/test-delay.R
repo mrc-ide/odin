e1d40bcfa5b68959b941c0c1c873f24d3f3565be
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-02-27 08:27:20 +0000

    Update test for delay packing
    Nobody should be relying on this

--- tests/testthat/port/test-delay.R
+++ tests/testthat/port/test-delay.R
@@ -213,7 +213,6 @@ test_that("3 arg delay with array", {
 ## This should also be done with a couple of scalars thrown in here
 ## too I think; they change things also.
 test_that("delay index packing", {
-  skip("delay offsets")
   gen <- odin2({
     deriv(a[]) <- i
     deriv(b[]) <- i
@@ -244,10 +243,10 @@ test_that("delay index packing", {
   seq0 <- function(n) seq_len(n)
 
   expect_equal(dat$dim_delay_foo, dat$dim_b + dat$dim_c + dat$dim_e)
-  expect_equal(dat$delay_idx_foo,
-               c(dat$offset_b + seq0(dat$dim_b),
-                 dat$offset_c + seq0(dat$dim_c),
-                 dat$offset_e + seq0(dat$dim_e)))
+  expect_equal(dat$delay_index_foo,
+               c(dat$dim_a + seq0(dat$dim_b),
+                 dat$offset_variable_c + seq0(dat$dim_c),
+                 dat$offset_variable_e + seq0(dat$dim_e)))
 
   tt <- seq(0, 10, length.out = 11)
   yy <- mod$transform_variables(mod$run(tt))
