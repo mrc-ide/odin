4122e4bd83f184a701b035e8b88a50a0b87b4786
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-02-28 13:34:26 +0000

    Add reference tests before we make schema changes

--- scripts/ir-build.R
+++ scripts/ir-build.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
 devtools::load_all()
-path <- "ir"
+path <- "tests/testthat/ir"
 re_ext <- "\\.R$"
 files <- dir(path, full.names = TRUE, pattern = re_ext)
 for (f in files) {
   message(f)
-  ir <- suppressMessages(odin_build_ir2(f, validate = TRUE))
+  ir <- suppressMessages(odin_build_ir2(f, pretty = FALSE, validate = TRUE))
   writeLines(ir, sub(re_ext, ".json", f))
 }
---
+++ tests/testthat/test-ir.R
@@ -0,0 +1,16 @@
+context("ir")
+
+## NOTE: this is too harsh a test but might come in useful
+test_that("reference sets are unchanged", {
+  path <- "ir"
+  re_ext <- "\\.json$"
+  files <- dir(path, full.names = TRUE, pattern = re_ext)
+
+  for (f_j in files) {
+    f <- sub(re_ext, ".R", f_j)
+    ir <- as.character(suppressMessages(
+      odin_build_ir2(f, pretty = FALSE, validate = TRUE)))
+    cmp <- trimws(readChar(f_j, file.size(f_j)))
+    expect_equal(ir, cmp)
+  }
+})
