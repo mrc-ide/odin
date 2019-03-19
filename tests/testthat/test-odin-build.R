629249c1cf94ccc15333eaf141ab6a8d08bcf865
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-03-19 07:48:45 +0000

    Skip output test on appveyor

--- tests/testthat/test-odin-build.R
+++ tests/testthat/test-odin-build.R
@@ -9,7 +9,9 @@ test_that("build from validate", {
   res <- odin_build(x, options)
   expect_is(res$model, "odin_generator")
   expect_is(res$output, "character")
-  expect_gte(length(res$output), 1L)
+  if (!on_appveyor()) {
+    expect_gte(length(res$output), 1L)
+  }
   expect_identical(res$ir, x$result)
   expect_true(res$success)
   expect_null(res$error)
@@ -25,7 +27,9 @@ test_that("build from ir", {
   res <- odin_build(x, options)
   expect_is(res$model, "odin_generator")
   expect_is(res$output, "character")
-  expect_gte(length(res$output), 1L)
+  if (!on_appveyor()) {
+    expect_gte(length(res$output), 1L)
+  }
   expect_identical(res$ir, x)
   expect_true(res$success)
   expect_null(res$error)
