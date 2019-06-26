e3c9cebd343195ede64ea346ee3ed4fb52754d9a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-06-26 07:03:56 +0100

    Disable validation

--- tests/testthat/helper-odin.R
+++ tests/testthat/helper-odin.R
@@ -1,5 +1,5 @@
 options(odin.verbose = FALSE,
-        odin.validate = requireNamespace("jsonvalidate", quietly = TRUE),
+        odin.validate = FALSE,
         odin.target = NULL)
 
 on_appveyor <- function() {
