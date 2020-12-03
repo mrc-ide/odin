faa9b06e532f7e420a93885e092be8cf83159b7e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-12-03 18:19:43 +0000

    Add test for read_include setting

--- tests/testthat/test-odin-options.R
+++ tests/testthat/test-odin-options.R
@@ -6,3 +6,10 @@ test_that("can create placeholder handler for include parsing", {
     opts$read_include(),
     "'config(include)' is not supported for target 'fortran'", fixed = TRUE)
 })
+
+
+test_that("manually set parsing functions persist", {
+  opts <- odin_options(target = "fortran")
+  opts$read_include <- read_include_c
+  expect_identical(odin_options(options = opts)$read_include, read_include_c)
+})
