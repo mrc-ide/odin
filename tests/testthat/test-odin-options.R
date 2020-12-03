e380df24dafa13139624ac03536b7428acbfa5a0
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2020-12-03 12:49:14 +0000

    Add test for unhandled include parse

---
+++ tests/testthat/test-odin-options.R
@@ -0,0 +1,8 @@
+context("odin_options")
+
+test_that("can create placeholder handler for include parsing", {
+  opts <- odin_options(target = "fortran")
+  expect_error(
+    opts$parse_include(),
+    "'config(include)' is not supported for target 'fortran'", fixed = TRUE)
+})
