0c2fb21f19189497572c88ebe33bfa1395bd83d1
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-03-09 09:50:55 +0000

    Tidy up test

--- tests/testthat/test-parse2-rewrite.R
+++ tests/testthat/test-parse2-rewrite.R
@@ -15,10 +15,7 @@ test_that("log", {
 })
 
 
-
-
-test_that("rewrite arrays", {
-  options <- odin_options(rewrite_dims = TRUE, validate = TRUE)
+test_that("rewrite arrays drops references to dim_ variables", {
   ir <- odin_parse({
     n <- 2
     m <- 2
@@ -29,51 +26,7 @@ test_that("rewrite arrays", {
     initial(I) <- 0
     S0[, ] <- user()
     dim(S0) <- c(n, m)
-  }, options = options)
-  gen <- odin_generate(ir, options)
-  mod <- gen(S0 = matrix(runif(4), 2, 2))
-  mod$run(0:10)
-
-  options <- odin_options(validate = TRUE)
-  ir <- odin_parse({
-    n <- 2
-    m <- 2
-    deriv(S[, ]) <- 0
-    deriv(I) <- S[n, m]
-    dim(S) <- c(n, m)
-    initial(S[, ]) <- S0[i, j]
-    initial(I) <- 0
-    S0[, ] <- user()
-    dim(S0) <- c(n, m)
-  }, options = options)
-
-  odin_generate(ir, options)
-
-  ir_deserialise(ir)
-
+  }, options = odin_options(rewrite_dims = TRUE))
   expect_false(grepl("dim_S_1", ir))
   expect_false(grepl("dim_S", ir))
-})
-
-
-test_that("rewrite arrays with shared dimensions", {
-  options <- odin_options(rewrite_dims = TRUE, validate = FALSE)
-  ir <- odin_parse({
-    n <- user(integer = TRUE)
-    m <- user(integer = TRUE)
-    deriv(x[, ]) <- 0
-    deriv(y[, ]) <- 0
-    initial(x[, ]) <- 0
-    initial(y[, ]) <- 0
-    dim(x) <- c(n, m)
-    dim(y) <- c(n, m)
-  }, options = options)
-  gen <- odin_generate(ir, options)
-  mod <- gen(n = 4, m = 5)
-  mod$contents()
-
-  static_eval(quote(a * 2 * 3))
-  static_eval(quote(2 * 3 * a))
-
-
 })
