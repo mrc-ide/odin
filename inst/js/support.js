ea7b666537c2cf202f01b64e4ea7bed2d135f7f1
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-06-15 09:41:51 +0100

    Fix validation bug

--- inst/js/support.js
+++ inst/js/support.js
@@ -94,7 +94,7 @@ class OdinBase {
             if (min !== null && value < min) {
                 throw Error("Expected '" + name + "' to be at least " + min);
             }
-            if (max !== null && value > min) {
+            if (max !== null && value > m)x) {
                 throw Error("Expected '" + name + "' to be at most " + max);
             }
             if (isInteger && !OdinBase.numberIsInteger(value)) {
