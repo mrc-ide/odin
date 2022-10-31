a7934626b6ee015c54e5efe60809a2fd1fcfca1c
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-10-31 11:07:23 +0000

    Fix for discrete time runner

--- inst/js/test-discrete.js
+++ inst/js/test-discrete.js
@@ -1,3 +1,4 @@
 function call_odin_bundle(Model, pars, tStart, tEnd, dt, nParticles) {
-    return dust.wodinRunDiscrete(Model, pars, tStart, tEnd, dt, nParticles)
+    const solution = dust.wodinRunDiscrete(Model, pars, tStart, tEnd, dt, nParticles);
+    return solution({mode: "grid", tStart, tEnd, nPoints: Infinity});
 }
