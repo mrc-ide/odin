791103d9f94eca72285dbf0e2c0808bd69925cd3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-06-26 13:32:57 +0100

    Add explanation

--- tests/testthat/helper-differentiate.R
+++ tests/testthat/helper-differentiate.R
@@ -6,7 +6,14 @@ expectation_continuous <- function(fd, pars, from, to) {
 }
 
 
-## Discrete distrbutions are somewhat harder
+## Discrete distrbutions are somewhat harder. Take fd (the density 'd'
+## function, e.g. dbinom) and fq (the corresponding quantile 'q'
+## function, e.g., qbinom) and work out some suitably far out value
+## that we capture at least 1-tol of the probability mass, then sum
+## over that. This is not quite an infinite sum but at tolerance of
+## 1e-12 we're around the limits of what we'd get summing over many
+## floating point numbers (and this is only used in tests with a
+## looser tolerance anyway)
 expectation_discrete <- function(fd, fq, pars, tol = 1e-12) {
   end <- do.call(fq, c(list(p = 1 - tol), unname(pars)))
   n <- seq(0, end, by = 1)
